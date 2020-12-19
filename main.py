fe15445335e2969fa25c4c00ba4c693823039a59
Egor Belousov <egor0288354@yandex.ru>
2020-12-19 12:52:50 +0300

    Restructure app: add Application class & move Entry to a separate file

--- main.py
+++ main.py
@@ -1,90 +1,37 @@
 import sys
-from hashlib import sha256
 
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLineEdit
-from entry_ui import Ui_MainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
 from sql_manager import SqlManager
+from entry import Entry
 
 
-def get_hash(string: str) -> str:
-    m = sha256()
-    m.update(bytes(string, encoding='utf8'))
-    return m.hexdigest()
-
-
-def check_password(password: str) -> bool:
-    if len(password) < 6:
-        return False
-    if password.lower() == password or password.upper() == password:
-        return False
-    if not any(map(lambda x: x.isdigit(), password)):
-        return False
-    return True
-
-
-class Entry(QMainWindow, Ui_MainWindow):
+class Application:
     def __init__(self):
-        super().__init__()
-        self.setupUi(self)
-        self.setupButtons()
-        self.centerWindow()
-        self.setFocus()
         self.sql_manager = SqlManager('music.db')
+        self.current_window = Entry(self)
+        self.current_window.show()
+        self.centerCurrentWindow()
 
-    def setupButtons(self):
-        self.regToLogButton.clicked.connect(
-            lambda: self.pageSwitch(self.stackedWidget, 1)
-        )
-        self.logToRegButton.clicked.connect(
-            lambda: self.pageSwitch(self.stackedWidget, 0)
-        )
-        self.regButton.clicked.connect(self.registerUser)
-        self.logButton.clicked.connect(self.loginUser)
+    def changeWindow(self, window: QMainWindow):
+        self.current_window.close()
+        self.current_window = window
+        self.current_window.show()
+        self.centerCurrentWindow()
 
-    def centerWindow(self):
-        frame_geometry = self.frameGeometry()
+    def centerCurrentWindow(self):
+        window = self.current_window
+        frame_geometry = window.frameGeometry()
         desktop = QApplication.desktop()
         screen = desktop.screenNumber(desktop.cursor().pos())
         center_point = desktop.screenGeometry(screen).center()
         frame_geometry.moveCenter(center_point)
-        self.move(frame_geometry.topLeft())
-
-    def pageSwitch(self, stacked_widget, index):
-        for obj in [self.regForm, self.logForm][abs(index - 1)].children():
-            if isinstance(obj, QLineEdit):
-                obj.setText('')
-        stacked_widget.setCurrentIndex(index)
-        self.setFocus()
-
-    def registerUser(self):
-        username = self.regUsernameInput.text()
-        password = self.regPasswordInput.text()
-        confirm = self.confirmInput.text()
-        email = self.emailInput.text()
-        if '' in [username, password, confirm, email]:
-            return
-        if password != confirm or not check_password(password):
-            return
-        self.sql_manager.add_user(username, get_hash(password), email)
-        self.pageSwitch(self.stackedWidget, 1)
-
-    def loginUser(self):
-        username = self.logUsernameInput.text()
-        password = self.logPasswordInput.text()
-        if '' in [username, password]:
-            return
-        user = self.sql_manager.get_user(username)
-        if not user:
-            return
-        if user[0]['password_hash'] != get_hash(password):
-            return
-        print('Login successfully!')
+        window.move(frame_geometry.topLeft())
 
 
 def main():
     app = QApplication(sys.argv)
-    en = Entry()
-    en.show()
+    my_app = Application()
     sys.exit(app.exec())
 
 
