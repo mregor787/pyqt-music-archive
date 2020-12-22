a2d3def55df2274af757fe0f9329203563b6b5f2
Egor Belousov <egor0288354@yandex.ru>
2020-12-22 19:43:20 +0300

    Configure all buttons on the profile page

--- main_window.py
+++ main_window.py
@@ -1,3 +1,8 @@
+from PIL import Image
+
+from PyQt5.QtWidgets import QFileDialog
+from PyQt5.QtGui import QPixmap
+
 from window import Window
 from main_window_ui import Ui_MainWindow
 
@@ -7,9 +12,49 @@ class MainWindow(Window, Ui_MainWindow):
         super().__init__(app)
 
     def setupButtons(self):
-        self.toProfileButton.clicked.connect(
-            lambda: self.pageSwitch(self.stackedWidget, 1)
-        )
+        self.toProfileButton.clicked.connect(self.toProfile)
         self.profileBackButton.clicked.connect(
             lambda: self.pageSwitch(self.stackedWidget, 0)
         )
+        self.profileInfoSaveButton.clicked.connect(self.profileSaveInfo)
+        self.privacySaveButton.clicked.connect(self.privacySaveInfo)
+        self.changeIconButton.clicked.connect(self.profileChangeIcon)
+
+    def toProfile(self):
+        self.pageSwitch(self.stackedWidget, 1)
+        user = self.sql_manager.get_user(self.app.getUser())
+        data = {
+            'email': self.profileEmailInput,
+            'first_name': self.profileFirstNameInput,
+            'last_name': self.profileLastNameInput,
+            'country': self.profileCountryInput,
+            'date_of_birth': self.profileBirthdateInput
+        }
+        for key in data:
+            data[key].setText(user[key])
+        (self.publicButton if user['profile_type'] == 'public'
+         else self.privateButton).setChecked(True)
+        self.profileIconPic.setPixmap(QPixmap(user['icon_path']))
+
+    def profileSaveInfo(self):
+        update_data = {
+            'email': self.profileEmailInput.text(),
+            'first_name': self.profileFirstNameInput.text(),
+            'last_name': self.profileLastNameInput.text(),
+            'country': self.profileCountryInput.text(),
+            'date_of_birth': self.profileBirthdateInput.text()
+        }
+        self.sql_manager.update_user(self.app.getUser(), update_data)
+
+    def privacySaveInfo(self):
+        profile_type = 'public' if self.publicButton.isChecked() else 'private'
+        self.sql_manager.update_user(self.app.getUser(), {'profile_type': profile_type})
+
+    def profileChangeIcon(self):
+        source = QFileDialog.getOpenFileName(self, 'Select Image', '', "Image (*.png *.jpg *.jpeg)")[0]
+        if not source:
+            return
+        destination = 'images/profile_icon.png'
+        Image.open(source).save(destination)
+        self.profileIconPic.setPixmap(QPixmap(destination))
+        self.sql_manager.update_user(self.app.getUser(), {'icon_path': destination})
